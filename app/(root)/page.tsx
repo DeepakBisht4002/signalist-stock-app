885acd6f9df5a2e40be1082b51571f185cd61623
Deepak Bisht <deepak.bisht4002@gmail.com>
2025-10-24 16:25:57 +0530

    Implement header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button"
 
 const Home = () => {
   return (
-    <>
-      <Button>Next</Button>
-    </>
+    <div className="flex min-h-screen home-wrapper">
+      Home
+    </div>
   )
 }
 
